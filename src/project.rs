e5298022776edebe225c84a1a3db03c055ab16f2
Hector Peeters <hector.peeters@gmail.com>
2021-08-15 13:18:22 +0200

    Clean up project.rs and add comments

--- src/project.rs
+++ src/project.rs
@@ -114,7 +114,6 @@ impl Project {
                     );
                 }
 
-                // Add any remaining user-specified args
                 if let Some(args) = &config.compiler_opts {
                     command.push_args(args);
                 }
@@ -126,15 +125,86 @@ impl Project {
         commands
     }
 
-    pub fn build(&self, cache: &mut Cache, config: &Config) -> Result<(), std::io::Error> {
-        // Gathering source files
-        let source_files = self.get_source_files();
+    pub fn link(&self, source_files: Vec<DirEntry>, config: &Config) -> Result<(), std::io::Error> {
+        // Determine the output directory
+        let output_dir = Path::new(&config.bin);
 
+        // Create the output directory if it doesn't exist
+        std::fs::create_dir_all(&output_dir)?;
+
+        // Get a list of all the object files
         let object_files: Vec<_> = source_files
             .iter()
             .map(|x| self.get_output_file(x.path(), config))
             .collect();
 
+        // Get the link command based on what kind of project this is
+        let mut link_command = match self.kind {
+            ProjectKind::Executable => {
+                let mut link_command = Command::new(&config.linker);
+
+                // Add all the object files that have to be linked
+                link_command.args(object_files);
+
+                // Add any other user-specified linker options
+                if let Some(args) = &config.linker_opts {
+                    link_command.args(args);
+                }
+
+                // Set up the output file main search directory
+                link_command
+                    .arg("-o")
+                    .arg(&output_dir.join(&self.name))
+                    .arg("-L")
+                    .arg(&config.bin);
+
+                // Add all the other project dependencies it has to link to
+                if let Some(deps) = &self.depends {
+                    link_command.args(deps.iter().map(|x| format!("-l{}", x)).collect::<Vec<_>>());
+                }
+
+                link_command
+            }
+            ProjectKind::Library => {
+                let mut link_command = Command::new(&config.packer);
+
+                // Setup the output file name and object files
+                link_command
+                    .arg("rcs")
+                    .arg(output_dir.join(format!("lib{}.a", self.name)))
+                    .args(object_files);
+
+                link_command
+            }
+        };
+
+        // Add all the linked libraries
+        if let Some(links) = &self.links {
+            link_command.args(links.iter().map(|x| format!("-l{}", x)).collect::<Vec<_>>());
+        }
+
+        // Execute the command and get the output
+        let output = link_command.output().expect("failed to link command");
+
+        // If the link command didn't exit succesfully, print the error and exit
+        if !output.status.success() {
+            eprintln!(
+                "{}{}{}",
+                color::Fg(color::Red),
+                String::from_utf8(output.stderr).unwrap(),
+                color::Fg(color::Reset),
+            );
+            std::process::exit(-1);
+        }
+
+        Ok(())
+    }
+
+    pub fn build(&self, cache: &mut Cache, config: &Config) -> Result<(), std::io::Error> {
+        // Gathering source files
+        let source_files = self.get_source_files();
+
+        // Check which source files we actually have to recompile
         let source_files_to_recompile: Vec<_> = source_files
             .iter()
             .filter(|x| {
@@ -150,116 +220,65 @@ impl Project {
             })
             .collect();
 
-        // Compiling source files
-
+        // If there is nothing to compile we don't show progress bars or link
         if source_files_to_recompile.is_empty() {
             return Ok(());
         }
 
+        // Set up the progress bar
         let progress_bar = ProgressBar::new(source_files_to_recompile.len() as u64);
         progress_bar.set_style(
             ProgressStyle::default_bar().template("{prefix:10} {bar:80} {pos:>5}/{len:5} {msg}"),
         );
         progress_bar.set_prefix(self.name.clone());
 
+        // Fetch all the compile commands
         let compile_commands = self.get_compile_commands(
             &source_files_to_recompile
                 .iter()
-                .map(|x| (**x).clone())
+                .map(|x| (*x).clone())
                 .collect::<Vec<_>>()[..],
             config,
         );
 
+        // Execute all compile commands
         for mut compile_command in compile_commands {
-            progress_bar.set_message(compile_command.file.to_string());
+            // Set the current file we are compiling
+            progress_bar.set_message(
+                compile_command
+                    .source_file
+                    .file_name()
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
+            );
 
+            // Get the output file and create it's parent directory if it doesn't exist
             let output_file = self.get_output_file(compile_command.source_file.path(), config);
             std::fs::create_dir_all(&output_file.parent().unwrap())?;
 
             match compile_command.execute() {
                 Ok(_) => {
+                    // The command executed succesfully so we can update the build cache
                     cache.update(&output_file);
                     cache.write()?;
+                    // Increment the progress bar
                     progress_bar.inc(1);
                 }
                 Err(e) => {
+                    // The command failed so lets print an error message
                     eprintln!("{}{}{}", color::Fg(color::Red), e, color::Fg(color::Reset),);
+                    // Since compilation stops here, we can stop the progress bar
                     progress_bar.finish_and_clear();
+                    // And exit the program
                     std::process::exit(-1);
                 }
             }
         }
+        // Compilation succesful
         progress_bar.finish_with_message("done");
 
-        // Linking compiled source files
-
-        let output_file = Path::new(&config.bin);
-
-        std::fs::create_dir_all(&output_file)?;
-
-        match self.kind {
-            ProjectKind::Executable => {
-                let mut link_command = Command::new(&config.linker);
-                link_command.args(object_files);
-                if let Some(args) = &config.linker_opts {
-                    link_command.args(args);
-                }
-                link_command
-                    .arg("-o")
-                    .arg(&output_file.join(&self.name))
-                    .arg("-L")
-                    .arg(&config.bin);
-
-                if let Some(links) = &self.links {
-                    for link in links {
-                        link_command.arg(format!("-l{}", link));
-                    }
-                }
-                if let Some(deps) = &self.depends {
-                    for dep in deps {
-                        link_command.arg(format!("-l{}", dep));
-                    }
-                }
-
-                let output = link_command.output().expect("failed to link command");
-
-                if !output.status.success() {
-                    eprintln!(
-                        "{}{}{}",
-                        color::Fg(color::Red),
-                        String::from_utf8(output.stderr).unwrap(),
-                        color::Fg(color::Reset),
-                    );
-                    std::process::exit(-1);
-                }
-            }
-            ProjectKind::Library => {
-                let mut link_command = Command::new(&config.packer);
-                link_command
-                    .arg("rcs")
-                    .arg(output_file.join(format!("lib{}.a", self.name)))
-                    .args(object_files);
-
-                if let Some(links) = &self.links {
-                    for link in links {
-                        link_command.arg(format!("-l{}", link));
-                    }
-                }
-
-                let output = link_command.output().expect("failed to link command");
-
-                if !output.status.success() {
-                    eprintln!(
-                        "{}{}{}",
-                        color::Fg(color::Red),
-                        String::from_utf8(output.stderr).unwrap(),
-                        color::Fg(color::Reset),
-                    );
-                    std::process::exit(-1);
-                }
-            }
-        }
-
-        Ok(())
+        // Link all compiled object files
+        self.link(source_files, config)
     }
 }
